6318fee2ac86cc821b41d082f6fd1bf6541db4eb
ramazanToksoz <rtoksoz7@gmail.com>
2024-05-07 14:54:19 +0300

    workspace create

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const fm=function name() {
-    console.log("dfdfd");
-}
+const express = require("express");
+const path = require("path");
+const ejs = require('ejs');
+const app = express();
+const port = 3000;
+
+/* const mylogger= (req,res,next) => {
+  console.log("selam dunya");
+  next()
+} */
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+  res.render("index");
+  
+});
+app.get("/index", (req, res) => {
+  res.render("index");
+  
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.listen(port, (res, req) => {
+  console.log(port + " port dinleniyor");
+});
